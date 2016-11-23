759b45b482e1954c9590bb786d7b19afdf73b04f
shnobel <dmytrovoronov@gmail.com>
2016-11-24 01:40:18 +0200

    Add GuestResponce model

--- src/PartyInvites/Controllers/HomeController.cs
+++ src/PartyInvites/Controllers/HomeController.cs
@@ -15,23 +15,11 @@ namespace PartyInvites.Controllers
             return View("MyView");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
+        public ViewResult RsvpForm()
         {
             return View();
         }
+
+      
     }
 }
